b9236e9bb4ec8df567e7dbfa40a3d1714cd874cc
yo16 <xxyo1xx@gmail.com>
2021-07-25 11:43:30 +0900

    ログを作った

--- src/fig_package/common/const.py
+++ src/fig_package/common/const.py
@@ -2,5 +2,11 @@
 固定値
 """
 
+# ログフォルダ
+LOGGER_DIR = './log'
+# ログレベル
+from logging import INFO, DEBUG
+LOG_LEVEL = DEBUG
+
 # BasicReaderクラス の save_original_data メソッドで保存する先
 PROCESSED_FILE = 'processed_file'
--- src/fig_package/common/funcs.py
+++ src/fig_package/common/funcs.py
@@ -1,8 +1,13 @@
 """ 共通関数
 """
+import datetime
+from logging import Logger, getLogger, StreamHandler, handlers, Formatter, \
+    INFO
+import os
 
-from logging import getLogger, StreamHandler, DEBUG
-def get_default_logger(initial_talk:bool=False):
+from .const import LOGGER_DIR, LOG_LEVEL
+
+def get_default_logger(initial_talk:bool=False) -> Logger:
     """
     デフォルトロガーを返す
 
@@ -12,14 +17,42 @@ def get_default_logger(initial_talk:bool=False):
         デフォルトロガーを使用したことをログ出力するか否か。
     """
     logger = getLogger(__name__)
-    handler = StreamHandler()
-    handler.setLevel(DEBUG)
-    logger.setLevel(DEBUG)
-    logger.addHandler(handler)
-    logger.propagate = False
+    # RotatingFileHandler版ロガー
+    __set_logger_RotatingFileHander(logger)
 
     if initial_talk:
         logger.debug('Using default logger.')
-
+        logger.debug(f'Log level is {logger.level}.')
+    
     return logger
+
+
+def __set_logger_StreamHandler(logger: Logger) -> None:
+    """
+    StreamHandler版ロガー
+    """
+    logger = getLogger(__name__)
+    handler = StreamHandler()
+    handler.setLevel(LOG_LEVEL)
+    logger.setLevel(LOG_LEVEL)
+    logger.addHandler(handler)
+    logger.propagate = False
+    
+
+def __set_logger_RotatingFileHander(logger: Logger) -> None:
+    """
+    RotatingFileHandler版ロガー
+    """
+    dt_now = datetime.datetime.now()
+    str_now = dt_now.strftime('%Y%m%d_%H%M%S')
+    str_log_dir = dt_now.strftime(f'{LOGGER_DIR}/%Y/%Y%m')
+    os.makedirs(str_log_dir, exist_ok=True)
+    handler = handlers.RotatingFileHandler(
+        f'{str_log_dir}/{str_now}.log', 'a+',
+        maxBytes=10000, backupCount=5
+    )
+    handler.setLevel(LOG_LEVEL)
+    handler.setFormatter(Formatter('[%(asctime)s] %(levelname)s: %(message)s'))
+    logger.addHandler(handler)
+    logger.setLevel(LOG_LEVEL)
 
